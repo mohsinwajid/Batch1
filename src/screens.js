4390053e64b5114a17551dc85b515fb20c0563f1
mohsinwajid <mohsinwajid414@gmail.com>
2023-08-23 16:37:53 +0500

    Screen file is added

---
+++ src/screens.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const screens = () => {
+  return (
+    <View>
+      <Text>screens</Text>
+    </View>
+  )
+}
+
+export default screens
+
+const styles = StyleSheet.create({})
