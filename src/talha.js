cc2eeb30520916e9846c9a8ceebc6d474c996a27
mohsinwajid <mohsinwajid414@gmail.com>
2023-08-23 16:40:17 +0500

    File added by talha

---
+++ src/talha.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const talha = () => {
+  return (
+    <View>
+      <Text>talha</Text>
+    </View>
+  )
+}
+
+export default talha
+
+const styles = StyleSheet.create({})
